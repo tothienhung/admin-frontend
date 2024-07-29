f6a075c1e9f52b8bc1ce16fe2e5259c9900d3095
ToThienHung-NLU <20130274@st.hcmuaf.edu.vn>
2024-07-29 21:38:18 +0700

    done

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -17,17 +17,15 @@ function LoginPage() {
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="w-[450px]  max-w-md p-8 space-y-6 bg-white rounded-md shadow-md">
 
-        <h1 className="text-2xl font-bold text-center text-black   text-[35px]  leading-[14px]">DEVTO</h1>
+        <h1 className="font-['Public_Sans'] text-center text-black font-black   text-[35px]  leading-[14px]">DEVTO</h1>
         <div className="space-y-[6px]">
-          <h1 className="text-[22px] font-semibold   leading-[30px] text-[var(--Light-Typography-Color-Heading-Text,#4B465C)]">
-            Welcome!
-          </h1>
-          <h1 className="text-[15px] font-normal   leading-[22px]">
+          <h1 className="text-[var(--Light-Typography-Color-Heading-Text,#4B465C)]  font-semibold font-['Public_Sans'] text-[22px] leading-[30px]    ">Welcome!</h1>
+          <h1 className="font-['Public_Sans'] text-[15px] font-normal  text-[var(--Light-Typography-Color-Body-Text,#4B465C)] leading-[22px]">
             Please sign in to your account and start the adventure
           </h1>
         </div>        <form onSubmit={handleSubmit} className="space-y-4">
           <div>
-            <label htmlFor="username" className="block text-sm font-medium text-gray-700">Email or Username</label>
+            <label htmlFor="username" className="font-['Public_Sans'] block text-[13px] font-normal text-gray-700">Email or Username</label>
             <input
               type="text"
               id="username"
@@ -40,8 +38,8 @@ function LoginPage() {
           </div>
           <div className="relative">
             <div className="flex items-center justify-between">
-              <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
-              <a href="#" className="text-sm text-indigo-500 hover:text-indigo-600">Forgot Password?</a>
+              <label htmlFor="password" className="font-['Public_Sans'] block text-[13px] font-normal text-gray-700">Password</label>
+              <a href="#" className="font-['Public_Sans']  text-[13px]  text-indigo-500 hover:text-indigo-600">Forgot Password?</a>
             </div>
             <input
               type="password"
@@ -54,26 +52,26 @@ function LoginPage() {
             />
           </div>
           <div className="flex items-center">
-            <input type="checkbox" className="w-5 h-5 text-indigo-600 transition duration-150 ease-in-out form-checkbox" />
-            <h1 className="ml-2 text-gray-900">Remember me</h1>
+            <input type="checkbox" className="w-[18px] h-[18px] text-indigo-600 transition duration-150 ease-in-out form-checkbox" />
+            <h1 className="ml-2 font-['Public_Sans'] text-[15px] font-normal  text-[var(--Light-Typography-Color-Body-Text,#4B465C)] leading-[22px]">Remember me</h1>
           </div>
           <button
             type="submit"
-            className="w-full px-4 py-2 text-white bg-indigo-600 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            className="w-full font-['Public_Sans'] px-4 py-2 text-white bg-indigo-600 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
           >
             Sign in
           </button>
           <div className="flex justify-center mt-8">
-            <p className="text-gray-700 ">
+            <p className="text-gray-700 font-['Public_Sans'] text-[15px] font-normal  text-[var(--Light-Typography-Color-Body-Text,#4B465C)] leading-[22px] ">
               New on our platform?
-              <a href="#" className="ml-1 font-bold text-blue-500 hover:text-blue-700">
+              <a href="#" className="ml-1   text-blue-500 hover:text-blue-700 font-['Public_Sans'] text-[15px] font-normal   leading-[22px]">
                 Create an account
               </a>
             </p>
           </div>
           <div className="flex items-center my-6">
             <div className="flex-grow border-t border-gray-400"></div>
-            <span className="flex-shrink mx-4 text-gray-500">or</span>
+            <span className="flex-shrink mx-4 font-['Public_Sans'] text-gray-500">or</span>
             <div className="flex-grow border-t border-gray-400"></div>
           </div>
           <div className="flex justify-center space-x-4">
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'public-sans-regular': ['Public Sans', 'sans-serif'],
+        'public-sans-bold': ['Public Sans', 'sans-serif'],
+      },
+      fontWeight: {
+        'public-sans-regular': 400,
+        'public-sans-bold': 700,
+      }
+    },
   },
   plugins: [],
 }
